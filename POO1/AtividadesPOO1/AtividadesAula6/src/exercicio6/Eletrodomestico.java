891ba6349620959fc53343e6c7f866476541d712
Rafael Fraga <rafalfraga@gmail.com>
2025-01-03 21:00:00 -0300

    corrige nome da classe para seguir padrao Java

---
+++ POO1/AtividadesPOO1/AtividadesAula6/src/exercicio6/Eletrodomestico.java
@@ -0,0 +1,51 @@
+package exercicio6;
+
+public class Eletrodoméstico implements Produto {
+    private static int idEletrodomesticos = 1;
+    private int anoProdução;
+    private int unidadeVenda;
+    private String categoria;
+    private float preco;
+    private int idLocal;
+
+    public Eletrodoméstico(int anoProdução, int unidadeVenda, String categoria, float preco) {
+        this.anoProdução = anoProdução;
+        this.unidadeVenda = unidadeVenda;
+        this.categoria = categoria;
+        this.preco = preco;
+        this.idLocal = idEletrodomesticos;
+        idEletrodomesticos++;
+    }
+
+    @Override
+    public String getID() {
+        return anoProdução + "-" + idLocal;
+    }
+
+    @Override
+    public int getAnoProdução() {
+        return anoProdução;
+    }
+
+    @Override
+    public int getUnidadeVenda() {
+        return unidadeVenda;
+    }
+
+    @Override
+    public String getCategoria() {
+        return categoria;
+    }
+
+    @Override
+    public float getPreço() {
+        return preco;
+    }
+
+    @Override
+    public String mostra() {
+        return "Eletrodoméstico: ID=" + getID() + ", Ano de Produção=" + getAnoProdução() +
+                ", Unidade de Venda=" + getUnidadeVenda() + ", Categoria=" + getCategoria() +
+                ", Preço=" + getPreço();
+    }
+}
