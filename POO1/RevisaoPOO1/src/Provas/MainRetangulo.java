8fcdd4ecae95a2c5d9c3a6b81d347499e2550e5b
Rafael Fraga <rafalfraga@gmail.com>
2025-02-04 21:00:00 -0300

    subindo classe que serviu de revisao para a prova

---
+++ POO1/RevisaoPOO1/src/Provas/MainRetangulo.java
@@ -0,0 +1,13 @@
+package Provas;
+
+//import java.util.Scanner;
+
+public class MainRetangulo {
+    public static void main(String[] args) {
+        //Scanner sc = new Scanner(System.in);
+        Retangulo r = new Retangulo(8,2);
+
+        System.out.println(r.calculaArea());
+        //System.out.println(r.calculaPerimetro());
+    }
+}
