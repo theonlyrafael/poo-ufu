43a8cf0c28c7df19fe2aab1d50d094b75d919235
Rafael Fraga <rafalfraga@gmail.com>
2025-02-18 15:15:00 -0300

    Sexta classe

---
+++ POO2/AtividadesPOO2/Escola/src/Humanos/Pai.java
@@ -0,0 +1,59 @@
+package Humanos;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pai extends Homem {
+
+    protected String[] filhos;
+    protected boolean trabalha;
+    protected List<String> listaTarefas;
+    protected List<Float> contas;
+
+    public Pai(String nome, String tipoOlhos, String cor, float altura, int idade,
+               String[] filhos, boolean trabalha) {
+        super(nome, tipoOlhos, cor, altura, idade);
+
+        this.filhos = filhos;
+        this.trabalha = trabalha;
+        this.listaTarefas = new ArrayList<>();
+        this.contas = new ArrayList<>();
+    }
+
+    public void imprimir(List<?> lista) {
+        for (Object item : lista) {
+            System.out.println(item);
+        }
+    }
+
+    public void imprimir(List<?> lista, String titulo) {
+        System.out.println(titulo);
+        imprimir(lista);
+    }
+
+    public void addTarefa(String tarefa) {
+        listaTarefas.add(tarefa);
+    }
+
+    public void addContas(float conta) {
+        contas.add(conta);
+    }
+
+
+    public String[] getFilhos() {
+        return filhos;
+    }
+
+    public boolean isTrabalha() {
+        return trabalha;
+    }
+
+    public List<String> getListaTarefas() {
+        return listaTarefas;
+    }
+
+    public List<Float> getContas() {
+        return contas;
+    }
+}
+
