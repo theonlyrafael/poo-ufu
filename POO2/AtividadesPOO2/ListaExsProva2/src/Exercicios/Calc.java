2b598a4ab2b6b9651d48dcd1436265c864b55232
Rafael Fraga <rafalfraga@gmail.com>
2025-02-18 15:15:00 -0300

    Sobe classe calculadora

---
+++ POO2/AtividadesPOO2/ListaExsProva2/src/Exercicios/Calc.java
@@ -0,0 +1,19 @@
+package Exercicios;
+
+public class Calc {
+    public int soma(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public int sub(int num1, int num2) {
+        return num1 - num2;
+    }
+
+    public int div(int num1, int num2) {
+        return num1 / num2;
+    }
+
+    public int multi(int num1, int num2) {
+        return num1 * num2;
+    }
+}
